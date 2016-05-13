f895825172b04a08094d99f7af133af1ee3d4940
everett980 <everett980@gmail.com>
2016-05-12 23:05:59 -0400

    makes modal somewhat prettier, resets stored input after submission, removes unneeded error statement

--- browser/src/components/ModalWrapper/ModalWrapper.js
+++ browser/src/components/ModalWrapper/ModalWrapper.js
@@ -11,29 +11,42 @@ class ModalWrapper extends Component {
 	addWhite() {
 		if(this.rule && this.rule.length) {
 			this.props.addRule(this.rule+'-Y');
-		} else {
-			this.wasError = true;
-		}
+			this.rule = null;
+			this.props.closeModal();
+		} 
 	}
 	addBlack() {
 		if(this.rule && this.rule.length) {
 			this.props.addRule(this.rule+'-N');
-		} else {
-			this.wasError = true;
-		}
+			this.rule = null;
+			this.props.closeModal();
+		} 
 	}
 	render() {
 		//I need to figure out how to have initial data in text input
 		return (
-				<Modal isOpen={this.props.open} onRequestClose={this.props.closeModal}>
-				<p>Add Rule:</p>
-				<input type='text' onChange={::this.recordRules}/>
+				<Modal isOpen={this.props.open} onRequestClose={this.props.closeModal} style={{
+					overlay: {
+								 backgroundColor: 'rgba(0, 0, 0, .75)'
+					},
+			   		content: {
+			   					 width: '50%',
+			   					 height: '80%',
+			   					 position: 'absolute',
+			   					 top: '10%',
+			   				     left: '25%'
+					}
+				}}>
+				<h3 style={{marginTop: '0px'}}>Add Rule:</h3>
+				<p>Add rules in the format of OuterRule.MiddleRule.InnerRule</p>
+				<p>You can chain as many requirements as you want.</p>
+				<p>Example, an IfStatement inside of a for loop inside of a while loop would look like: 'while.for.ifstatement'</p>
+				<p>Adding to white list means that the provided code structure must exist, adding to black list means that the provided code structure must not exist.</p>
+				<input type='text' onChange={::this.recordRules} style={{width: 'calc(100% - 16px)', padding: '5px', margin: '8px'}}/>
 				<br/>
-				<button onClick={::this.addWhite}>Add to White List</button>
-				<button onClick={::this.addBlack}>Add to Black List</button>
-				<br/>
-				{this.wasError ? 'There was an error, please ensure there is a rule entry and then try again.' : ''}
-				<button onClick={this.props.closeModal}>Cancel</button>
+				<button onClick={::this.addWhite} style={{color: 'black', backgroundColor: 'white', border: '2px solid darkgrey', padding: '5px', margin: '8px'}}>Add to White List</button>
+				<button onClick={::this.addBlack} style={{color: 'white', backgroundColor: 'black', border: '2px solid darkgrey', padding: '5px', margin: '8px'}}>Add to Black List</button>
+				<button onClick={this.props.closeModal} style={{color: 'black', backgroundColor: '#4169E1', border: '2px solid darkgrey', padding: '5px', margin: '8px'}}>Cancel</button>
 				</Modal>
 			   );
 	}
